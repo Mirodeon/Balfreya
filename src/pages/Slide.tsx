031e3c111119bbd498d8895031b3c404a5f157f9
Mirodeon <mehdi.habaru@gmail.com>
2023-06-13 14:26:15 +0200

    feat(slider):add addclassname and removeclassname, add animation, add timeout

--- src/pages/Slide.tsx
+++ src/pages/Slide.tsx
@@ -2,11 +2,14 @@ import { useSelector } from "react-redux";
 import { RootState } from "../store";
 import data from "../img"
 import { useState } from "react";
+import useTimeOut from "../utils/useTimeOut";
+import { addClassName, removeClassName } from "../utils/addClassName";
 
 const Slide = () => {
     const account = useSelector((state: RootState) => state.auth.account);
     document.title = "Balfreya - Slide" + (account ? ` : ${account.username}` : "");
     const [slide, setSlide] = useState(0);
+    const [direction, setDirection] = useState<"next" | "prev" | null>(null)
 
     const prev = () => {
         return slide !== 0 ? slide - 1 : data.length - 1;
@@ -15,16 +18,32 @@ const Slide = () => {
         return slide !== data.length - 1 ? slide + 1 : 0;
     }
 
+    useTimeOut(
+        () => {
+            removeClassName(direction === "next" ? "move_next" : "move_prev", ".container_img")
+            setSlide(direction === "next" ? next() : prev());
+            setDirection(null)
+        },
+        direction ? 700 : null
+    );
+
     return (
         <main className="container_slide">
-            <div className="container_img">
-                <img className="img_prev" src={data[prev()]} alt="slide" />
-                <img className="img_slide" src={data[slide]} alt="slide" />
-                <img className="img_next" src={data[next()]} alt="slide" />
+            <div className="container_slide_img">
+                <div className="container_img prev">
+                    <img className="img_slide" src={data[prev()]} alt="slide" />
+                </div>
+                <div className="container_img">
+                    <img className="img_slide" src={data[slide]} alt="slide" />
+                </div>
+                <div className="container_img next">
+                    <img className="img_slide" src={data[next()]} alt="slide" />
+                </div>
             </div>
             <div className="container_button">
                 <div className="nav_btn" onClick={() => {
-                    setSlide(prev())
+                    addClassName("move_prev", ".container_img")
+                    setDirection("prev")
                 }}>
                     <span></span>
                     <span></span>
@@ -33,7 +52,8 @@ const Slide = () => {
                     Prev
                 </div>
                 <div className="nav_btn" onClick={() => {
-                    setSlide(next())
+                    addClassName("move_next", ".container_img")
+                    setDirection("next")
                 }}>
                     <span></span>
                     <span></span>
---
+++ src/utils/addClassName.ts
@@ -0,0 +1,25 @@
+export const addClassName = (className: string, selector: string, index?: number) => {
+    const node: NodeListOf<Element> = document.querySelectorAll(selector);
+    if (index) {
+        if (node[index]) {
+            node[index].classList.add(className);
+        }
+    } else {
+        node.forEach(
+            elem => elem.classList.add(className)
+        )
+    }
+};
+
+export const removeClassName = (className: string, selector: string, index?: number) => {
+    const node: NodeListOf<Element> = document.querySelectorAll(selector);
+    if (index) {
+        if (node[index]) {
+            node[index].classList.remove(className);
+        }
+    } else {
+        node.forEach(
+            elem => elem.classList.remove(className)
+        )
+    }
+};
